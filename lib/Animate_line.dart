d99eadb87537b4a4ebe789f47ad6058a49ec8bae
thetechgirlgita <thetechgirlgita@gmail.com>
2022-02-23 16:08:14 +0545

    Alphabet A in islad

--- lib/Animate_line.dart
+++ lib/Animate_line.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:path_drawing/path_drawing.dart';
 
 Color colorThree = Color(0xffCE7C00).withOpacity(0.8);
@@ -49,36 +50,66 @@ class AnimateLine extends StatelessWidget {
 class PathPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+
+
     Paint paint = Paint()
       ..color = Color(0xffCA7A03)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 10.0;
 
-
     Path path = Path();
-    path.moveTo(size.width*0.2, size.height*0.9);
-    path.cubicTo(0, size.height*1, size.width*0.3, size.height*0.9, size.width*0.5, size.height*0.5);
-    path.lineTo(size.width*0.8, size.height*0.1);
+    path.moveTo(size.width * 0.2, size.height * 0.9);
+    path.cubicTo(0, size.height * 1, size.width * 0.3, size.height * 0.9,
+        size.width * 0.5, size.height * 0.5);
+    path.lineTo(size.width * 0.8, size.height * 0.1);
 
     canvas.drawPath(
         dashPath(
           path,
-          dashArray: CircularIntervalList<double>(<double>[28.0, 8.5]), // Width and height of dashed line
+          dashArray: CircularIntervalList<double>(
+              <double>[28.0, 8.5]), // Width and height of dashed line
         ),
         paint);
 
     path = Path();
-    Offset circle = Offset(size.width*0.2, size.height*0.9);
+    Offset circle = Offset(size.width * 0.2, size.height * 0.9);
     path.close();
     paint.color = Colors.green;
     paint.style = PaintingStyle.fill;
     canvas.drawCircle(circle, 40, paint);
 
     path = Path();
-    Offset Circle = Offset(size.width*0.8, size.height*0.1);
+    Offset Circle = Offset(size.width * 0.8, size.height * 0.1);
     paint.color = Colors.green;
     paint.style = PaintingStyle.fill;
-    canvas.drawCircle(Circle, 40, paint);
+    TextSpan span =
+        new TextSpan(style: new TextStyle(color: Colors.grey[600]), text: 'A');
+    canvas.drawCircle(
+      Circle,
+      40,
+      paint,
+    );
+    final textspan = TextSpan(
+      text: 'A',
+      style:
+      GoogleFonts.shadowsIntoLight(
+
+        color: Colors.white,
+        fontSize: 50,
+      ),
+    );
+    final textPainter = TextPainter(
+      text: textspan,
+
+      textDirection: TextDirection.rtl,
+    );
+    textPainter.layout(
+      minWidth: 10,
+      maxWidth: size.width,
+    );
+    final offset = Offset(size.width*0.76, size.height*0.04);
+
+    textPainter.paint(canvas, offset);
   }
 
   @override
